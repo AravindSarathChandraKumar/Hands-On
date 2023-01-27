181523b9307532ace55ff6c92fa65d680924ac05
AravindSarathChandraKumar <aravinds95@gmail.com>
2023-01-27 16:15:40 +0530

    Modified Banner and Nav styles

--- netflix-clone/netflix-clone/src/Components/Banner/Banner.js
+++ netflix-clone/netflix-clone/src/Components/Banner/Banner.js
@@ -3,8 +3,16 @@ import './Banner.css'
 
 function Banner() {
   return (
-    <div className='banner container col-sm-12 col-md-12'>
-
+    <div className='banner container-fluid col-sm-12'>
+        <div class="col-lg-6 content align-items-center justify-content-center border-primary">
+            <div className="h2 title text-white ">
+                Movie Title Here
+            </div>
+            <div className="p des text-white">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, impedit!
+            </div>
+            <button  type="button" className="btn btn-outline-light btn-large mt-2">More</button>
+        </div>
     </div>
   )
 }
